13eb1d5442e9393251a96e18d24ccc571fbd7674
Christoph Witzko <github@christophwitzko.com>
2015-08-26 14:30:19 +0200

    fix: removed logging

--- index.js
+++ index.js
@@ -5,11 +5,8 @@
 module.exports = function (hoodie, doneCallback) {
 
   hoodie.task.on('test:add', handleNewTest)
-  console.log('#YOLO handling new test tasks?')
 
   function handleNewTest (dbName, test) {
-    console.log('handling new test task in %s', dbName)
-    console.log(test)
     if (test.fail) {
       hoodie.task.error(dbName, test, {error: 'TestError', message: 'Test failed errored intentionally.'})
     } else {
